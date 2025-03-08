efa76e4b5e9114fee1b8488a99fcd0c936c812ff
Vladislav <wlad.bologov@gmail.com>
2025-03-08 15:43:54 +0300

    task_No._1_solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,21 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d == 0) {
+    arr.push(-b / (2 * a));
+  }
+
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+  }
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+//solveEquation(1, 2, 10);
+
+//function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+//
+//}
