d2359da0c8402f17386a3d490a1c24b41c3cc595
Vladislav <wlad.bologov@gmail.com>
2025-03-27 22:52:53 +0300

    Error_ 3_ point

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,26 @@
 function Student(name, gender, age) {
-  
+  this.name = name,
+    this.gender = gender,
+    this.age = age,
+    this.marks = []
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (student1.hasOwnProperty('marks')) {
+    student1.marks.push(...marks);
+  }
+
+
 }
 
 Student.prototype.getAverage = function () {
-  
+
 }
 
 Student.prototype.exclude = function (reason) {
-  
+
 }
