394b7fd049847a51be3a806dfe394fdb855fd6ee
Vladislav <wlad.bologov@gmail.com>
2025-04-17 13:34:15 +0300

    Вопрос по геттерам

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -21,20 +21,26 @@ class Triangle {
         this.a = a,
             this.b = b,
             this.c = c
+        if (this.a > this.b + this.c || this.b > this.a + this.c || this.c > this.a + this.b) {
+            throw new Error("Треугольник с такими сторонами не существует")
+        }
 
 
-    }
+    };
 
 
 
     get perimeter() {
-        let P = this.a + this.b + this.c
-        return P
+        let P = this.a + this.b + this.c;
+        return Number(P);
     }
 
     get area() {
-        let S = Math.sqrt(P * (P - this.a) * (P - this.b) * (P - this.c))
-        return S.toFixed(3)
+        let semiperimeter = 0.5(this.a + this.b + this.c);
+
+        let S = Math.sqrt(semiperimeter * (semiperimeter - this.a) * (semiperimeter - this.b) * (semiperimeter - this.c));
+
+        return Number(S.toFixed(3));
     }
 
 
